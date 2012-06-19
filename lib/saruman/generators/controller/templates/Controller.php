750568e97d254be52d4acee137e0808e7c0821b3
Jason Ayre <jasonayre@gmail.com>
2012-06-18 20:11:02 -0600

    bug fix controller load

--- lib/saruman/generators/controller/templates/Controller.php
+++ lib/saruman/generators/controller/templates/Controller.php
@@ -6,9 +6,11 @@ class <%= @controller.klass_name %> extends Mage_Core_Controller_Front_Action
   <%= action.visibility %> function <%= action.method_name %>()
   {
     <% if @controller.create_views %>
+    
     $params = $this->getRequest()->getParams();
+    $this->loadLayout();    
     $this->renderLayout();
-    $this->loadLayout();
+
     <% else %>
       echo "Hello world.";
       print_r($params);    
